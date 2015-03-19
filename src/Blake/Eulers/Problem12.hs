08419db42e3c9e4769a8a3174f75c2b2bf798b8a
Blake Harley <blake@blakeharley.com>
2015-03-19 01:15:01 -0500

    Problem 12

---
+++ src/Blake/Eulers/Problem12.hs
@@ -0,0 +1,14 @@
+module Blake.Eulers.Problem12 where
+
+import Data.Numbers.Primes (primeFactors)
+import Blake.Eulers.Utils (takeWhileInclusive, frequency)
+
+answer = last $ takeWhileInclusive ((<=500) . numberOfDivisors) [triangleNumber i | i <- [7..]]
+
+-- Finds the requested triangle number
+triangleNumber :: Int -> Int
+triangleNumber n = (n * (n + 1)) `div` 2
+
+-- Get the number of divisors
+numberOfDivisors :: Int -> Int
+numberOfDivisors = product . map ((+1) . fst) . frequency . primeFactors
