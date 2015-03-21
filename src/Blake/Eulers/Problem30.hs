27f4bc7e62ce188f7d669f5779e82070ea9f35f2
Blake Harley <blake@blakeharley.com>
2015-03-21 17:48:15 -0500

    Problem 30

---
+++ src/Blake/Eulers/Problem30.hs
@@ -0,0 +1,8 @@
+module Blake.Eulers.Problem30 where
+
+import Blake.Eulers.Utils (digits)
+
+answer = sum $ filter isDigit5thPower [11..1000000]
+
+isDigit5thPower :: Int -> Bool
+isDigit5thPower n = (sum $ map (^5) $ digits n) == n
